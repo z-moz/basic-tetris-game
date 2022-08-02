87a292e0b7f2ff53587a54a3b7a22b2b0e436b4f
zsuzsanna mozsary <mozsaryzsuzsanna@gmail.com>
2022-08-02 17:21:00 +0100

    changed grid design

--- js/script.js
+++ js/script.js
@@ -10,75 +10,93 @@
 
 // drawTeeshape();
 
-const squares = document.querySelectorAll(".square");
-let a = 0;
-let b = 0;
-let c = 0;
-let d = 0;
-let squareColor = "yellow";
+// const squares = document.querySelectorAll(".square");
+// let a = 0;
+// let b = 0;
+// let c = 0;
+// let d = 0;
+// let squareColor = "yellow";
 
-function rotation1() {
-  a = 14;
-  b = 15;
-  c = 16;
-  d = 17;
+// function rotation1() {
+//   a = 14;
+//   b = 15;
+//   c = 16;
+//   d = 17;
+// }
+
+// function rotation2() {
+//   a -= 8;
+//   b += 1;
+//   c += 10;
+//   d += 19;
+// }
+
+// function rotation3() {
+//   a += 21;
+//   b += 10;
+//   c -= 1;
+//   d -= 12;
+// }
+
+// function rotation4() {
+//   a += 8;
+//   b -= 1;
+//   c -= 10;
+//   d -= 19;
+// }
+
+// function rotation5() {
+//   a -= 21;
+//   b -= 10;
+//   c += 1;
+//   d += 12;
+// }
+
+// function spawnBlock() {
+//   squares[a].classList.add(squareColor);
+//   squares[b].classList.add(squareColor);
+//   squares[c].classList.add(squareColor);
+//   squares[d].classList.add(squareColor);
+// }
+
+// rotation1();
+// rotation2();
+// spawnBlock();
+
+// function removeBlock() {
+//   squares[a].classList.remove(squareColor);
+//   squares[b].classList.remove(squareColor);
+//   squares[c].classList.remove(squareColor);
+//   squares[d].classList.remove(squareColor);
+// }
+
+// function dropBlock() {
+//   removeBlock();
+//   a += 10;
+//   b += 10;
+//   c += 10;
+//   d += 10;
+//   spawnBlock();
+// }
+
+// dropBlock();
+// dropBlock();
+// dropBlock();
+
+const height = 24;
+const width = 10;
+const board = document.querySelector("#board");
+
+let grid = new Array(width);
+for (let i = 0; i < width; i++) {
+  grid[i] = new Array(height);
+  for (let j = 0; j < height; j++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    grid[i][j] = cell;
+    board.append(cell);
+  }
 }
-
-function rotation2() {
-  a -= 8;
-  b += 1;
-  c += 10;
-  d += 19;
-}
-
-function rotation3() {
-  a += 21;
-  b += 10;
-  c -= 1;
-  d -= 12;
-}
-
-function rotation4() {
-  a += 8;
-  b -= 1;
-  c -= 10;
-  d -= 19;
-}
-
-function rotation5() {
-  a -= 21;
-  b -= 10;
-  c += 1;
-  d += 12;
-}
-
-function spawnBlock() {
-  squares[a].classList.add(squareColor);
-  squares[b].classList.add(squareColor);
-  squares[c].classList.add(squareColor);
-  squares[d].classList.add(squareColor);
-}
-
-rotation1();
-rotation2();
-spawnBlock();
-
-function removeBlock() {
-  squares[a].classList.remove(squareColor);
-  squares[b].classList.remove(squareColor);
-  squares[c].classList.remove(squareColor);
-  squares[d].classList.remove(squareColor);
-}
-
-function dropBlock() {
-  removeBlock();
-  a += 10;
-  b += 10;
-  c += 10;
-  d += 10;
-  spawnBlock();
-}
-
-dropBlock();
-dropBlock();
-dropBlock();
+console.log(grid);
+grid[0][23].classList.add("yellow");
+grid[9][23].classList.add("yellow");
