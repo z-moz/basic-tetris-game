ad1e589292c65b00773a598a1b4dc047d64bbba7
zsuzsanna mozsary <mozsaryzsuzsanna@gmail.com>
2022-08-05 17:35:36 +0100

    added wallkick, floor kick, check for other blocks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const rows = 24;
 const columns = 10;
 const board = document.querySelector("#board");
+let oldAx;
+let oldBx;
+let oldCx;
+let oldOx;
+let oldAy;
+let oldBy;
+let oldCy;
+let oldOy;
 
 let grid = new Array(rows);
 for (let i = 0; i < rows; i++) {
@@ -12,7 +20,7 @@ for (let i = 0; i < rows; i++) {
     board.append(cell);
   }
 }
-// coordinates are: grid[y][x] or grid[row][column]
+// NOTE order of coordinates: grid[y][x] or grid[row][column]
 
 // grid[0][8].classList.add("yellow");
 // grid[0][9].classList.add("yellow");
@@ -20,16 +28,15 @@ for (let i = 0; i < rows; i++) {
 // grid[7][5].classList.add("yellow");
 // grid[7][7].classList.add("yellow");
 
-// function switchRowToYellow(row) {
-//   grid[row].forEach((element) => {
-//     element.classList.add("yellow");
-//     element.classList.toggle("empty");
-//   });
-// }
+function switchRowToYellow(row) {
+  grid[row].forEach((element) => {
+    element.classList.add("yellow");
+    element.classList.remove("empty");
+  });
+}
 
-// switchRowToYellow(4);
-// switchRowToYellow(11);
-// switchRowToYellow(23);
+switchRowToYellow(11);
+switchRowToYellow(23);
 
 class Block {
   constructor(Ax, Ay, Bx, By, Cx, Cy, Ox, Oy, color) {
@@ -49,79 +56,135 @@ class Block {
     grid[this.By][this.Bx].classList.add(this.color);
     grid[this.Cy][this.Cx].classList.add(this.color);
     grid[this.Oy][this.Ox].classList.add(this.color);
-    grid[this.Ay][this.Ax].classList.toggle("empty");
-    grid[this.By][this.Bx].classList.toggle("empty");
-    grid[this.Cy][this.Cx].classList.toggle("empty");
-    grid[this.Oy][this.Ox].classList.toggle("empty");
+    grid[this.Ay][this.Ax].classList.remove("empty");
+    grid[this.By][this.Bx].classList.remove("empty");
+    grid[this.Cy][this.Cx].classList.remove("empty");
+    grid[this.Oy][this.Ox].classList.remove("empty");
   }
-  removeBlock() {
+  deleteBlock() {
     grid[this.Ay][this.Ax].classList.remove(this.color);
     grid[this.By][this.Bx].classList.remove(this.color);
     grid[this.Cy][this.Cx].classList.remove(this.color);
     grid[this.Oy][this.Ox].classList.remove(this.color);
-    grid[this.Ay][this.Ax].classList.toggle("empty");
-    grid[this.By][this.Bx].classList.toggle("empty");
-    grid[this.Cy][this.Cx].classList.toggle("empty");
-    grid[this.Oy][this.Ox].classList.toggle("empty");
+    grid[this.Ay][this.Ax].classList.add("empty");
+    grid[this.By][this.Bx].classList.add("empty");
+    grid[this.Cy][this.Cx].classList.add("empty");
+    grid[this.Oy][this.Ox].classList.add("empty");
   }
-  dropBlock() {
-    this.removeBlock();
-    this.Ay += 1;
-    this.By += 1;
-    this.Cy += 1;
-    this.Oy += 1;
-    this.drawBlock();
+  storeOrigXY() {
+    oldAx = this.Ax;
+    oldBx = this.Bx;
+    oldCx = this.Cx;
+    oldOx = this.Ox;
+    oldAy = this.Ay;
+    oldBy = this.By;
+    oldCy = this.Cy;
+    oldOy = this.Oy;
   }
-  // left: shiftBlock(-1), right: shiftBlock(1)
-  shiftBlock(direction) {
-    this.removeBlock();
-    this.Ax += direction;
-    this.Bx += direction;
-    this.Cx += direction;
-    this.Ox += direction;
-    this.drawBlock();
+  returnOrigXY() {
+    this.Ax = oldAx;
+    this.Bx = oldBx;
+    this.Cx = oldCx;
+    this.Ox = oldOx;
+    this.Ay = oldAy;
+    this.By = oldBy;
+    this.Cy = oldCy;
+    this.Oy = oldOy;
   }
-  wallKick() {
+  // to left: shiftXY(-1, 0), to right: shiftXY(1, 0), down: shiftXY(0, 1), up: shiftXY(0, -1)
+  shiftXY(a, b) {
+    this.Ax += a;
+    this.Bx += a;
+    this.Cx += a;
+    this.Ox += a;
+    this.Ay += b;
+    this.By += b;
+    this.Cy += b;
+    this.Oy += b;
+  }
+  // clockwise: rotateXY(-1,1), anticlockwise: rotateXY(1,-1)
+  rotateXY(a, b) {
+    this.Ax = a * (this.Ay - this.Oy) + this.Ox;
+    this.Ay = b * (oldAx - this.Ox) + this.Oy;
+    this.Bx = a * (this.By - this.Oy) + this.Ox;
+    this.By = b * (oldBx - this.Ox) + this.Oy;
+    this.Cx = a * (this.Cy - this.Oy) + this.Ox;
+    this.Cy = b * (oldCx - this.Ox) + this.Oy;
+  }
+  wallKickFloorKick() {
     while (this.Ax < 0 || this.Bx < 0 || this.Cx < 0 || this.Ox < 0) {
-      this.shiftBlock(1);
+      this.shiftXY(1, 0);
     }
     while (this.Ax > 9 || this.Bx > 9 || this.Cx > 9 || this.Ox > 9) {
-      this.shiftBlock(-1);
+      this.shiftXY(-1, 0);
+    }
+    while (this.Ay > 23 || this.By > 23 || this.Cy > 23 || this.Oy > 23) {
+      this.shiftXY(0, -1);
     }
   }
-
-  // clockwise: rotateBlock(-1,1), anticlockwise: rotateBlock(1,-1)
-  rotateBlock(m, n) {
-    this.removeBlock();
-    let oldAx = this.Ax;
-    let oldBx = this.Bx;
-    let oldCx = this.Cx;
-    this.Ax = m * (this.Ay - this.Oy) + this.Ox;
-    this.Ay = n * (oldAx - this.Ox) + this.Oy;
-    this.Bx = m * (this.By - this.Oy) + this.Ox;
-    this.By = n * (oldBx - this.Ox) + this.Oy;
-    this.Cx = m * (this.Cy - this.Oy) + this.Ox;
-    this.Cy = n * (oldCx - this.Ox) + this.Oy;
-    this.wallKick();
+  isCellEmpty() {
+    if (
+      grid[this.Ay][this.Ax].classList.contains("empty") &&
+      grid[this.By][this.Bx].classList.contains("empty") &&
+      grid[this.Cy][this.Cx].classList.contains("empty") &&
+      grid[this.Oy][this.Ox].classList.contains("empty")
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  moveBlock(a, b, operation) {
+    this.deleteBlock();
+    this.storeOrigXY();
+    switch (operation) {
+      case "rotate":
+        this.rotateXY(a, b);
+        break;
+      case "shift":
+        this.shiftXY(a, b);
+        break;
+    }
+    this.wallKickFloorKick();
+    if (!this.isCellEmpty()) {
+      this.returnOrigXY();
+    }
     this.drawBlock();
   }
   handleEvent(e) {
     switch (e.code) {
       case "ArrowUp":
-        this.rotateBlock(-1, 1);
+        this.moveBlock(-1, 1, "rotate");
         break;
       case "ArrowLeft":
-        this.shiftBlock(-1);
+        this.moveBlock(-1, 0, "shift");
         break;
       case "ArrowRight":
-        this.shiftBlock(1);
+        this.moveBlock(1, 0, "shift");
         break;
       case "ArrowDown":
-        this.dropBlock();
+        this.moveBlock(0, 1, "shift");
         // document.removeEventListener("keyup", this);
         break;
     }
   }
+  // handleEvent(e) {
+  //   switch (e.code) {
+  //     case "ArrowUp":
+  //       this.rotateBlock(-1, 1);
+  //       break;
+  //     case "ArrowLeft":
+  //       this.shiftBlock(-1, 0);
+  //       break;
+  //     case "ArrowRight":
+  //       this.shiftBlock(1, 0);
+  //       break;
+  //     case "ArrowDown":
+  //       this.shiftBlock(0, 1);
+  //       // document.removeEventListener("keyup", this);
+  //       break;
+  //   }
+  // }
 }
 
 function deleteFullRow() {
@@ -154,4 +217,4 @@ function deleteFullRow() {
 const yellowL = new Block(5, 2, 6, 2, 8, 2, 7, 2, "yellow");
 yellowL.drawBlock();
 
-deleteFullRow();
+// deleteFullRow();
